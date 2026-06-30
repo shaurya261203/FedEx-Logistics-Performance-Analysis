3a1842056ea0cc86678d2c1c9dc125a15f324cb6
shaurya261203 <shauryapat12@gmail.com>
2026-07-01 00:21:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="FedEx Dashboard", layout="wide")
+
+st.title("📦 FedEx Logistics Performance Analysis")
+
+st.write("Welcome to my Streamlit Dashboard.")
+
+st.header("Business Objective")
+
+st.write("""
+Analyze FedEx shipment data to improve logistics performance,
+delivery efficiency, and business decision making.
+""")
